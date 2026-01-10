5ae9b42c1689f0e614bc1c198dcfbbac8f138468
TI-GRUPOAURICA <ti@metalabsac.com>
2026-01-10 14:56:03 -0500

    web_helpdesk.py

--- web_helpdesk.py
+++ web_helpdesk.py
@@ -91,7 +91,7 @@ if menu == "📝 Reportar Incidencia":
 
     tipo_seleccion = st.radio(
         "¿Qué tipo de atención requiere?",
-        ["🛠 Soporte Técnico (Algo falla)", "📋 Solicitud / Requerimiento (Necesito algo nuevo)"],
+        ["🛠 Soporte Técnico (Algo falla)", "📋 Solicitud"],
         horizontal=True
     )
     
@@ -106,7 +106,7 @@ if menu == "📝 Reportar Incidencia":
             obra = st.text_input("Obra / Sede")
         with col2:
             if tipo_bd == "Soporte":
-                inventario = st.text_input("Cod de Inventario")
+                inventario = st.text_input("Cod de Inventario - MYJ-EI-XXX ")
             else:
                 st.info("🔹 Solicitud general (No requiere código de inventario)")
                 inventario = "N/A - Solicitud"
@@ -358,3 +358,4 @@ elif menu == "🔒 Panel Administrador":
         if password:
             st.error("Contraseña incorrecta")
         st.info("Ingrese la contraseña en la barra lateral.")
+
